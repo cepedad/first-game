c03356c8b7fd7c935b3c48f50450c7b79aead7d6
cepedad <cepedadmc@gmail.com>
2025-01-27 12:06:30 -0500

    Clean up 'jumps remaining' logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,12 +4,12 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 const MAX_JUMPS = 2
 
-@onready var jump_counter = 0
+@onready var jumps_remaining = MAX_JUMPS
 @onready var was_on_floor = false
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 
-func left_right_handler(was_on_floor: bool):
+func left_right_handler():
 	# Get input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
 	
@@ -38,9 +38,10 @@ func left_right_handler(was_on_floor: bool):
 
 func jump_handler():
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and jump_counter < MAX_JUMPS:
+	if Input.is_action_just_pressed("jump") and jumps_remaining > 0:
 		# Increment jump counter
-		jump_counter += 1
+		if not is_on_floor():
+			jumps_remaining -= 1
 		
 		# Play sound
 		jump_sound.play()
@@ -51,21 +52,22 @@ func jump_handler():
 
 func down_handler():
 	if Input.is_action_pressed("move_down") && is_on_floor():
+		# Move one pixel down (assuming platform collision boxes are all 1px thick)
 		position.y += 1
 		move_and_slide()
 
 func _physics_process(delta: float) -> void:
-	# Constantly add gravity
+	# ALWAYS: add gravity
 	velocity += get_gravity() * delta
 	
-	# Check jump conditions
+	# ALWAYS: check how many jumps left
 	if is_on_floor():
-		jump_counter = 0
-	if not is_on_floor() and was_on_floor:
-		jump_counter += 1
-
-	# Handle possible movements (jump, walking, falling thru platform, walking off ledge)
-	jump_handler()
+		jumps_remaining = MAX_JUMPS
+	if was_on_floor and not is_on_floor():
+		jumps_remaining -= 1
 	was_on_floor = is_on_floor()
-	left_right_handler(was_on_floor)
+
+	# Handle possible movements (walk or jump)
+	jump_handler()
+	left_right_handler()
 	down_handler()
